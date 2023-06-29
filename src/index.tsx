f31e0474e2ceabbfa11e9d82da2d84a4405e1b94
runheme <403688727@qq.com>
2023-06-29 17:55:10 +0800

    d
    d

--- src/index.tsx
+++
@@ -1,7 +0,0 @@
-import '@webscopeio/react-textarea-autocomplete/style.css';
-import 'rc-slider/assets/index.css';
-import React from 'react';
-import {render} from 'react-dom';
-import Root from './containers/Root';
-
-render(<Root />, document.getElementById('root'));
